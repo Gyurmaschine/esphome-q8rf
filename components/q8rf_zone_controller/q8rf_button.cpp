4586be048df06572c4d8326b3a50935f214ba7dc
Gyurmaschine <gyurmaschine@gmail.com>
2025-12-15 17:31:56 +0100

    fix

--- components/q8rf_zone_controller/q8rf_button.cpp
+++ components/q8rf_zone_controller/q8rf_button.cpp
@@ -1,52 +1,43 @@
-#include "esphome/core/log.h"
 #include "q8rf_button.h"
-#include "esphome/components/button/button_component.h"
+#include "esphome/core/log.h"
 
-namespace esphome
-{
-    namespace q8rf
-    {
+namespace esphome {
+namespace q8rf {
 
-        static const char *TAG = "q8rf.button";
+static const char *TAG = "q8rf.button";
 
-        void Q8RFButton::setup()
-        {
-            controller_->compile_msg(this->q8rf_device_id_, this->q8rf_zone_id_, Q8RF_MSG_CMD_PAIR, this->msg_pair_);
-            this->initialized_ = true;
-        }
-        void Q8RFButton::press_action()
-        {
+void Q8RFButton::setup() {
+  controller_->compile_msg(
+      q8rf_device_id_, q8rf_zone_id_, Q8RF_MSG_CMD_PAIR, msg_pair_);
+  initialized_ = true;
+}
 
-            if (!this->initialized_)
-            {
-                this->setup();
-            }
+void Q8RFButton::press_action() {
+  if (!initialized_) {
+    setup();
+  }
 
-            if (this->initialized_)
-            {
-                ESP_LOGI(TAG, "Send message PAIR device: 0x%04x zone: %d ", this->q8rf_device_id_, this->q8rf_zone_id_);
-                controller_->send_msg(msg_pair_);
-            }
-        }
+  ESP_LOGI(TAG, "Send PAIR device: 0x%04X zone: %d",
+           q8rf_device_id_, q8rf_zone_id_);
+  controller_->send_msg(msg_pair_);
+}
 
-        void Q8RFButton::set_q8rf_device_id(uint16_t id)
-        {
-            this->q8rf_device_id_ = id;
-        }
-        void Q8RFButton::set_q8rf_zone_id(uint16_t id)
-        {
-            this->q8rf_zone_id_ = id;
-        }
-        void Q8RFButton::set_controller(Q8RFController *controller)
-        {
-            this->controller_ = controller;
-        }
+void Q8RFButton::set_q8rf_device_id(uint16_t id) {
+  q8rf_device_id_ = id;
+}
 
-        void Q8RFButton::dump_config()
-        {
-            ESP_LOGCONFIG(TAG, "  Q8RF Device ID: 0x%04x", this->q8rf_device_id_);
-            ESP_LOGCONFIG(TAG, "  Q8RF Zone ID: %d", this->q8rf_zone_id_);
-        }
+void Q8RFButton::set_q8rf_zone_id(uint16_t id) {
+  q8rf_zone_id_ = id;
+}
 
-    } // namespace q8rf
-} // namespace esphome
+void Q8RFButton::set_controller(Q8RFController *controller) {
+  controller_ = controller;
+}
+
+void Q8RFButton::dump_config() {
+  ESP_LOGCONFIG(TAG, "Q8RF Device ID: 0x%04X", q8rf_device_id_);
+  ESP_LOGCONFIG(TAG, "Q8RF Zone ID: %d", q8rf_zone_id_);
+}
+
+}  // namespace q8rf
+}  // namespace esphome
